3b32272a8fb25d9a08e119977acb326883cbab27
brainwithE <haylyzh@gmail.com>
2023-09-07 01:42:22 +0800

    convert popup page to tsx

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
 import './Popup.css';
 
-const Popup = () => {
+const Popup: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
         </p>
